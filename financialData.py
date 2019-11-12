e70525dc9cb16c10e4e42aa85854716e9cc323c3
rrStein <rrstein521@gmail.com>
2019-11-12 22:46:53 +0000

    updated financial data fetching

--- financialData.py
+++ financialData.py
@@ -1,22 +1,40 @@
 import pandas as pd
 import math as m
 import numpy as np
-from datetime import datetime
+from datetime import datetime as dt
+import os
 import yfinance as yf
 from pandas_datareader import data as pdr
+from forex_python.converter import CurrencyRates, CurrencyCodes
+from forex_python.bitcoin import BtcConverter
+import world_bank_data as wb 
 
 # yf.pdr_override() #pandas datareader format
 
 ###### SET PARAMETERS ######
-today = datetime.today().strftime('%Y-%m-%d')
+today = dt.today().strftime('%Y-%m-%d')
 start_dt = "2015-01-01"
-period = "10y" #1d, 5d, 1mo,3mo,6mo,1y,2y,5y,10,ytd,max
+period = "1y" #1d, 5d, 1mo,3mo,6mo,1y,2y,5y,10,ytd,max
 interval = "1d" #1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
 
 ###### DEFINE STOCKS ######
-labels = 'goog aapl'
-tickers = yf.Tickers(labels)
+stocks = 'GOOG AAPL TSLA CL=F OPECX ^GSPC PE OAS GC=F WTI'
+rates = 'GBPUSD=X BTC-GBP'
+tickers = yf.Tickers(stocks)
+
+###### DEFINE CURRENCY RATES ###### 
+c = CurrencyRates()
+b = BtcConverter()
+c.get_rate("USD", "GBP")
 
 
-stocks = yf.download(tickers, period = period, interval = interval)
-stocks
+Stocks = yf.download(stocks, period = period, interval = interval)
+Rates = yf.download(rates, period = period, interval = interval)
+
+###### DATAHUB URLS ######
+
+###### WORLD BANK DATA ########
+wb.get_topics()
+wb.get_sources()
+wb.get_countries()
+# wb.get_series()
