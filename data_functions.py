e5a86ccbdc4adc36dd1a39ea86171378a8ea4995
rrStein <rrstein521@gmail.com>
2019-12-18 17:36:24 +0000

    Changed structure and started developing logistic regression

---
+++ data_functions.py
@@ -0,0 +1,253 @@
+import os.path
+import time
+import csv
+import shutil
+import urllib.error as ue
+import urllib.request as ur
+import pandas as pd
+import math as m
+import numpy as np
+from datetime import datetime as dt
+import os
+import yfinance as yf
+from pandas_datareader import data as pdr
+from IPython.display import display
+import requests
+yf.pdr_override()
+
+def show_more(df, lines):
+    with pd.option_context("display.max_rows", lines):
+        display(df)
+        
+
+def plot2axis(x, y1, y2, x_name, y_name, y2_name, lineax1 = False,\
+    lineax1y = 0, lineax1name = "lost", fill_boll = False, \
+    bol_low = 0, bol_high = 0, bol_name = "easy"):
+    plt.style.use("seaborn")
+    fig, ax = plt.subplots()
+    color = 'tab:green'
+    ax.set_xlabel(x_name, size=16)
+    ax.set_ylabel(y_name, size=16, color=color)
+    ax.plot(x, y1, lw=1, color=color, label = y_name)
+    ax.tick_params(axis='y', labelcolor=color)
+    if lineax1 == True:
+        ax.plot(x,lineax1y, label=lineax1name, linewidth =2, alpha =0.75, color="orange")
+    if fill_boll ==True:
+        ax.axhspan(bol_low, bol_high, color='blue', label=bol_name, alpha=0.75)
+
+    ax.legend(loc='upper left', prop={'size':16})
+    
+    
+    ax2 = ax.twinx()
+    color = 'tab:blue'
+    # we already handled the x-label with ax
+    ax2.set_ylabel(y2_name, size=14, color=color)
+    ax2.plot(x, y2, color=color, lw=1.5, label=y2_name)
+    ax2.tick_params(axis='y', labelcolor=color)
+    fig.tight_layout()
+    plt.show()
+    
+    
+# yf.pdr_override() #pandas datareader format
+
+""" YAHOO FINANCE SECTION START """
+###### SET PARAMETERS ######
+# def yFinData(period, interval, **stocks):
+
+# today = dt.today().strftime('%Y-%m-%d')
+# start_dt = "2015-01-01"
+# period = "5y" #1d, 5d, 1mo,3mo,6mo,1y,2y,5y,10,ytd,max
+# interval = "5d" #1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+
+# ###### DEFINE STOCKS ######
+# # Google, Apple, Tesla, Crude oil, S&P 500,Parsley Energy Inc.,Oasis Petroleum Inc., Gold, W&T Offshore Inc.,
+# # NASDAQ 100, Goodrich Petroleum Corporation
+# #stocks = 'BZ=F GOOG AAPL TSLA CL = F ^ GSPC PE OAS GC = F WTI NQ = F GDP'
+
+# stocks = "CL=F"  # CL=F ^GSPC OAS GDP ^DJI NQ=F"
+
+# # GBP/USD, BTC/GBP, USD/JPY, EUR/GBP, ETH/USD
+# # rates = 'GBPUSD=X CNY=X EURUSD=X'
+# tickers = yf.Ticker(stocks)
+# tickers.info
+# ###### GET DATA #######
+# Stocks = yf.download(stocks, period="max", interval="1d")
+# Stocks.columns
+# len(Stocks)
+# Rates = yf.download(rates, period = period, interval = interval)
+# show_more(Stocks, 300)
+
+""" YAHOO FINANCE SECTION END """
+
+###### DATAHUB URLS ######
+
+
+#US Oil Production in 1000 barrels per day
+def oilProduction(url='https://www.eia.gov/dnav/pet/hist_xls/WCRFPUS2w.xls'):
+    #url = 'https://www.eia.gov/dnav/pet/hist_xls/WCRFPUS2w.xls'
+    r2 = requests.get(url)
+    data_xls = pd.read_excel(url, 'Data 1', skiprows=2, comment='#')
+    data_xls.columns = ['Date', 'Production of Crude Oil']
+    #Oil_Production = data_xls
+    return data_xls
+
+
+""" DATAHUB SECTION START """
+
+
+def dataHub(url, import_new_data = True, fname = "WTI_oil_prices.csv", error = False):
+    #fname = 'WTI_oil_prices.csv'
+    # import_new_data = False
+
+    # error = False  # you can use this flag to tell the programme to basically skip everything else and go to the end so you don't get a crash when there's a problem
+
+    #Allows the user to choose to use a previously downloaded datafile, or to download a new one. Mostly so I don't have to keep downloading the same datafile over and over again
+    # if os.path.isfile(fname) == True:
+    #     while True:
+    #         user_selection = "Y" #input("Download new data (Y/N)? \n")
+    #         if user_selection == 'Y':
+    #             import_new_data = True
+    #             break
+    #         if user_selection == 'N':
+    #             import_new_data == False
+    #             break
+    #         else:
+    #             print("Input not recognised, please select either 'Y' or 'N'")
+    # else:
+    #     print("No local file detected, defaulting to downloading data")
+    #     import_new_data = True
+
+    if import_new_data == False:
+        print("Skipping download")
+    else:
+        print("Downloading csv file...")
+
+    if import_new_data == True:
+        try:
+            response = ur.urlopen(url)
+            print("Connection ok")
+            data = response.read()      # a `bytes` object
+            # a `str`; this step can't be used if data is binary
+            text = data.decode('utf-8')
+            with ur.urlopen(url) as response, open(fname, 'wb') as out_file:
+                shutil.copyfileobj(response, out_file)
+        except (ue.HTTPError, ue.URLError) as e:
+                requrl = url
+                print("Error, something went wrong with the download Error code:", e.code)
+                print("If the error code is '404', try checking the url and/or your internet connection - 404 means that it can't find the address")
+                error = True
+        if error == False:
+            print("Download Complete")
+
+    if error == False:
+
+        Dates_List = []
+        Prices_List = []
+
+        print("Reading downloaded csv file...")
+
+        with open(fname) as csvDataFile:
+            csvReader = csv.reader(csvDataFile)
+            for row in csvReader:
+                Dates_List.append(row[0])
+                Prices_List.append(row[1])
+
+            print("Reading complete, creating dataframe...")
+        p = np.asarray(Prices_List[1:]).astype(float)
+        d = np.asarray(Dates_List[1:]).astype('datetime64')
+        frame = pd.DataFrame({"Date": d, "Prices": p})
+        # frame = frame.drop([0])
+        #frame["Prices"] = pd.to_numeric(frame["Prices"], errors='coerce')
+
+        print("Dataframe created!")
+
+    #print("Programme complete!")
+    return frame
+
+""" DATAHUB SECTION END """
+
+""" WORLD BANK DATA SECTION START """
+###### EXPLORE WORLD BANK DATA ########
+# import world_bank_data as wb
+# wb.get_topics()
+# wb.get_sources()
+# countries = wb.get_countries()
+# inds = wb.get_indicators(topic = 7)
+# show_more(inds,300)
+
+###### GET THE DATA #######
+# ffConsumption = wb.get_series(
+#     "EG.USE.COMM.FO.ZS", date="2018", id_or_value='id', simplify_index=True)
+# gdp = wb.get_series(
+#     "NY.GDP.MKTP.CD", date="2018", id_or_value='id', simplify_index=True)
+# gdpPerCapita = wb.get_series(
+#     "NY.GDP.PCAP.CD", date="2018", id_or_value='id', simplify_index=True)
+# gni = wb.get_series(
+#     "NY.GNP.MKTP.CD", date="2018", id_or_value='id', simplify_index=True)
+# costOfDamageDueToCarbonEmissions = wb.get_series(
+#     "NY.ADJ.DCO2.CD", date="2018", id_or_value='id', simplify_index=True)
+
+# ####### CREATE PANDAS DATAFRAME #######
+# wbdf = countries[['region', 'name']].rename(columns={'name': 'country'}).loc[countries.region != 'Aggregates']
+# wbdf["year"] =
+# wbdf['ffConsumption'] = ffConsumption
+# wbdf["gdp"] = gdp
+# wbdf["gdpPerCapita"] = gdpPerCapita
+# wbdf["gni"] = gni
+# wbdf["costOfDamageDueToCarbonEmissions"] = costOfDamageDueToCarbonEmissions
+# show_more(wbdf, 218)
+""" WORLD BANK DATA SECTION END """
+#Stocks
+#frame.columns
+
+""" COMBINING DATA FRAMES """
+def combineFrames(dfPrice, df2): 
+    start_dt = np.max([np.min(dfPrice["Date"]), np.min(df2["Date"])])
+    
+    newframe = dfPrice[dfPrice["Date"] >= start_dt]
+    proddata = df2[df2["Date"] >= start_dt]
+
+
+# newframe = newframe.drop([0])
+# newframe = newframe.rename(columns={"Dates": "Date"})
+    newframe["Date"] = pd.to_datetime(newframe["Date"])
+
+# test = proddata
+# test["Date"] = pd.to_datetime(test.Date, format='%d/%m/%Y')
+# test.set_index('Date').resample('B').ffill().reset_index()
+
+    proddata = proddata.set_index('Date').resample('B').ffill().reset_index()
+
+    combined = pd.merge(proddata, newframe, how='outer', on='Date')
+    # combined.columns
+
+    nulls = combined[combined.isnull()]
+    last_prod = combined['Production of Crude Oil'].last_valid_index()
+    last_prod_val = combined['Production of Crude Oil'].iloc[last_prod]
+
+    oil = combined.pop("Production of Crude Oil")
+    oil = oil.fillna(last_prod_val)
+
+    combined["Production of Crude Oil"] = oil
+    return combined
+    # combined["Prices"] = pd.to_numeric(combined["Prices"])
+
+
+# p = np.asarray(final_frame["Prices"])
+# d = np.asarray(final_frame["Date"])
+
+# from datetime import date, timedelta
+# today = date.today()
+# gap = (today - date(2015, 1, 1)).days
+# from scipy.optimize import curve_fit
+# from scipy.interpolate import make_interp_spline, BSpline
+
+# x_new = [today - timedelta(days=x) for x in range(gap)]
+# spl = make_interp_spline(T, power, k=3)  # type: BSpline
+# power_smooth = spl(xnew)
+
+
+
+
+
+
--- logRegression.py
+++ logRegression.py
@@ -1,3 +1,20 @@
+import os.path
+import time
+import csv
+import shutil
+import pandas as pd
+import math as m
+import numpy as np
+from datetime import datetime as dt
+import os
+import yfinance as yf
+from pandas_datareader import data as pdr
+from IPython.display import display
+import requests
+yf.pdr_override()
+sys.path.insert(1, "/Users/qw19176/Documents/Courses/codingchallenge/")
+import data_functions as dataFun
+
 
 def SMA(period, data):
     sma = data.rolling(window=period).mean()
@@ -12,11 +29,16 @@ test = combined["Prices"]
 tes = test.dropna()
 
 
-combined["20dSMA"] = SMA(20, tes)
-combined["10dSMA"] = SMA(10, tes)
-combined["5dSMA"] = SMA(5, tes)
-combined["50dSMA"] = SMA(50, tes)
-combined["200dSMA"] = SMA(200, tes)
+priceDF = dataFun.dataHub(url="https://datahub.io/core/oil-prices/r/wti-daily.csv", import_new_data = False)
+oilDF = dataFun.oilProduction()
+df = dataFun.combineFrames(priceDF,oilDF)
+
+
+df["20dSMA"] = SMA(20, df["Prices"])
+df["10dSMA"] = SMA(10, df["Prices"])
+df["5dSMA"] = SMA(5, df["Prices"])
+df["50dSMA"] = SMA(50, df["Prices"])
+df["200dSMA"] = SMA(200, df["Prices"])
 
 
 def bolli_bois(data):
@@ -34,9 +56,14 @@ combined["bollinger_high"] = bolli_bois(combined["20dSMA"])[1]
 
 show_more(combined, 300)
 
-plot2axis(combined["Date"], combined["Prices"].astype(
+dataFun.plot2axis(combined["Date"], combined["Prices"].astype(
     float), combined["Production of Crude Oil"], "Date", "Price (USD)",
     'Production of Crude Oil (Thousand Barrels per Day)', lineax1=True,
     lineax1y=combined["20dSMA"], lineax1name="20d SMA",
     fill_boll=True, bol_high=combined["bollinger_high"],
     bol_low=combined["bollinger_low"])
+
+
+dataFun.plot2axis(combined["Date"], combined["Prices"].astype(
+    float), combined["Production of Crude Oil"], "Date", "Price (USD)",
+    'Production of Crude Oil (Thousand Barrels per Day)')
