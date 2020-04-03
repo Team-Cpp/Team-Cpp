3e690ef84b6374d4b573ddf42530ce58018dba86
eparrag1 <57716059+eparrag1@users.noreply.github.com>
2020-04-03 10:36:48 +0100

    Update Intellidock_GUI.py
    Moving tkinter sections from Chris Intellidock. Removing Test Accuracy Button.

--- Intellidock_GUI.py
+++ Intellidock_GUI.py
@@ -3,7 +3,7 @@ from tkinter import *
 
 import os
 import sys
-sys.path.insert(1,os.environ['DF_ROOT'])
+#sys.path.insert(1,os.environ['DF_ROOT'])
 
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, NavigationToolbar2Tk)
@@ -14,7 +14,7 @@ import matplotlib.patches as mpatches
 import numpy as np
 import pandas as pd
 
-from GUI.Chris_Intellidock import *
+from Chris_Intellidock import *
 
 
 barrels = 750000
@@ -55,7 +55,17 @@ def download_data():
 
 def Accuracy(df):
     window = Toplevel(root)      
-    Intellidock_Test_Accuracy(df,window,barrels,costPerDay)
+    string1,string2,string3,string4 = Intellidock_Test_Accuracy(df,barrels,costPerDay)
+    
+    lbl = Label(window, text=string1,font = ('Arial',30))
+    lbl.grid(column=0, row=0)    
+    lbl = Label(window, text=string2,font = ('Arial',30))
+    lbl.grid(column=0, row=1)    
+    lbl = Label(window, text=string3,font = ('Arial',30))
+    lbl.grid(column=0, row=2)    
+    lbl = Label(window, text=string4,font = ('Arial',30))
+    lbl.grid(column=0, row=3)  
+    
     
 def Train(df):
     window = Toplevel(root)  
@@ -76,21 +86,55 @@ def Train(df):
 
 def Predict(df,model,X_test,y_test):
     window = Toplevel(root)  
-
-    Intellidock_Predict_Next_Day(df,model,X_test,y_test,window,barrels,costPerDay)
+    
+    output_string,string1,string2,string3,string4,string5,string6,string7,string8 = Intellidock_Predict_Next_Day(df,model,X_test,y_test,barrels,costPerDay)
+    
+    lbl = Label(window, text= output_string,font = ('Arial',30))
+    lbl.grid(column=0, row=0) 
+        
+    lbl = Label(window, text="Details:",font = ('Arial',30))
+    lbl.grid(column=0, row=1)    
+    lbl = Label(window, text=string1,font = ('Arial',30))
+    lbl.grid(column=0, row=2)    
+    lbl = Label(window, text=string2,font = ('Arial',30))
+    lbl.grid(column=0, row=3)    
+    lbl = Label(window, text=string3,font = ('Arial',30))
+    lbl.grid(column=0, row=4)    
+    lbl = Label(window, text=string4,font = ('Arial',30))
+    lbl.grid(column=0, row=5)    
+    lbl = Label(window, text=string5,font = ('Arial',30))
+    lbl.grid(column=0, row=6)    
+    lbl = Label(window, text=string6,font = ('Arial',30))
+    lbl.grid(column=0, row=7)    
+    lbl = Label(window, text=string7,font = ('Arial',30))
+    lbl.grid(column=0, row=8)    
+    lbl = Label(window, text=string8,font = ('Arial',30))
+    lbl.grid(column=0, row=9)    
     
 
 def profit(df):
     window = Toplevel(root)  
 
-    Intellidock_Test_Profitability(df,window,barrels,costPerDay)
+    string1,string2,string3,string4 = Intellidock_Test_Profitability(df,barrels,costPerDay)
     
-    window2 = Toplevel(window)
+    lbl = Label(window, text=string1,font = ('Arial',30))
+    lbl.grid(column=0, row=0)    
+    lbl = Label(window, text=string2,font = ('Arial',30))
+    lbl.grid(column=0, row=1)    
+    lbl = Label(window, text=string3,font = ('Arial',30))
+    lbl.grid(column=0, row=2)    
+    lbl = Label(window, text=string4,font = ('Arial',30))
+    lbl.grid(column=0, row=3)    
+    
+    
+    window2 = Toplevel(root)
     canvas = Canvas(window2, width = 400, height = 300)      
     canvas.pack()          
     window2.img = img = PhotoImage(file="Deviation_Histogram.png")  
 
     canvas.create_image(0,0, anchor=NW, image=img)
+    
+    
 def features():
     window = Toplevel(root)  
     
@@ -109,23 +153,23 @@ btn = Button(root, text="Download Data",bg = 'green',command = download_data,fon
 btn.grid(column=0, row=1)
 
 
-btn = Button(root, text="Test the accuracy of the system",bg = 'green',command = Accuracy,font = ('Arial',30),state = 'disabled')
-btn.grid(column=0, row=2)
+#btn = Button(root, text="Test the accuracy of the system",bg = 'green',command = Accuracy,font = ('Arial',30),state = 'disabled')
+#btn.grid(column=0, row=2)
 
     
 btn = Button(root, text="Train the model (must do pre prediction)",bg = 'green',command = Train,font = ('Arial',30),state = 'disabled')
-btn.grid(column=0, row=3)
+btn.grid(column=0, row=2)
 
 
 btn = Button(root, text="Predict if staying undocked is worthwhile",bg = 'green',command = Predict,font = ('Arial',30),state = 'disabled')
-btn.grid(column=0, row=4)
+btn.grid(column=0, row=3)
 
     
 btn = Button(root, text="Run a profitability check",bg = 'green',command = profit,font = ('Arial',30),state = 'disabled')
-btn.grid(column=0, row=5)
+btn.grid(column=0, row=4)
 
 
 btn = Button(root, text="Show Feature Importance",bg = 'green',command = features,font = ('Arial',30),state = 'disabled')
-btn.grid(column=0, row=6)
+btn.grid(column=0, row=5)
 
 mainloop()
